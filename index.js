2e7c4c0422e037f61f3540a8445173a62b2a5ecc
Julia Bridges <julia@Julias-MacBook-Air.local>
2020-09-11 23:46:21 -0400

    fixed typo, someone please save me from myself.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { eventNames } = require("./data/dbConfig");
 const app = require("./server");
 
-const port = 4000 || eventNames.port.PORT;
+const port = 4000 || env.port.PORT;
 
 app.listen(port, () => {
     console.log("server is listening on port: ", port);
