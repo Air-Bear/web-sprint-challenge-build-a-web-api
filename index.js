06a371e473808f24c8618dce0c14b7afb9e873d5
Julia Bridges <julia@Julias-MacBook-Air.local>
2020-09-11 23:44:20 -0400

    set port to handle env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const { eventNames } = require("./data/dbConfig");
 const app = require("./server");
 
-const port = 4000;
+const port = 4000 || eventNames.port.PORT;
 
 app.listen(port, () => {
     console.log("server is listening on port: ", port);
