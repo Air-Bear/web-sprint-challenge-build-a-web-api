51368d4b87cd76b86e169243d60e21ef81fd42e7
Julia Bridges <julia@Julias-MacBook-Air.local>
2020-09-11 23:49:43 -0400

    third time isn't the charm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { eventNames } = require("./data/dbConfig");
 const app = require("./server");
 
-const port = 4000 || env.port.PORT;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
     console.log("server is listening on port: ", port);
